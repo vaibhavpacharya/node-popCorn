62835306dfeccdb77a5cb0b5d82e16cb82148ec6
Vaibhav Acharya <vaibhav.ach@gmail.com>
2020-05-17 11:33:57 -0700

    Remove unused packages (#6)
    Co-authored-by: Vaibhav Acharya <Vaibhav.Acharya@cognizant.com>

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
 const express = require('express');
 
 const router = express.Router();
-const moment = require('moment');
-const request = require('request');
-const bodyParser = require('body-parser');
 
 router.get('/', async (req, res, next) => {
   try {
